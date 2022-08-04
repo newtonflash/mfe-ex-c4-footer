820ddc4b97d8df749b8ef1583917a909caaa8684
Amanullah Khan <Patan.Khan@tesco.com>
2022-08-04 11:18:36 +0530

    add footer

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Footer from "./footer/Footer"
+
+const App = () => <Footer />;
+
+export default App;
---
+++ src/footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./styles.css";
+
+const Footer = () => (
+  <div className="footer">
+    <div className='logo'>LOGO</div>
+    <div className='links'>
+      <a href='#'>Sample Link 1</a>
+      <a href='#'>Sample Link 1</a>
+    </div>
+    <div className='copyright'>Copyright @www.samplewebsite.com</div>
+  </div>
+);
+
+export default Footer;
